d96f774f9df66f712c3e1c7306036e4b36b16805
Maxim Toyberman <maxtoyberman@gmail.com>
2019-07-08 13:56:48 +0300

    Rename package

---
+++ android/src/main/java/com/toyberman/drawOverlay/RNDrawOverlayModule.java
@@ -0,0 +1,68 @@
+package com.toyberman.drawOverlay;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.BaseActivityEventListener;
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class RNDrawOverlayModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+    private final int DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE = 1222;
+    private Promise mPromise;
+    private final ActivityEventListener mActivityEventListener = new BaseActivityEventListener() {
+        @Override
+        public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(activity, requestCode, resultCode, data);
+            if (requestCode == DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    if (Settings.canDrawOverlays(activity.getApplicationContext())) {
+                        // Permission Granted by Overlay
+                        mPromise.resolve(true);
+                    }
+                    else {
+                        mPromise.resolve(false);
+                    }
+                } else {
+                    mPromise.resolve(true);
+                }
+
+            }
+        }
+
+
+    };
+
+
+    public RNDrawOverlayModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+
+        this.reactContext.addActivityEventListener(mActivityEventListener);
+    }
+
+    @Override
+    public String getName() {
+        return "RNDrawOverlay";
+    }
+
+
+    @ReactMethod
+    public void askForDispalayOverOtherAppsPermission(Promise promise) {
+        mPromise = promise;
+        if (!Settings.canDrawOverlays(this.reactContext)) {
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + this.reactContext.getPackageName()));
+            this.reactContext.startActivityForResult(intent, DRAW_OVER_OTHER_APP_PERMISSION_REQUEST_CODE, null);
+        } else {
+            promise.resolve(true);
+        }
+    }
+}
--- android/src/main/java/com/toyberman/drawOverlay/RNDrawOverlayPackage.java
+++ android/src/main/java/com/toyberman/drawOverlay/RNDrawOverlayPackage.java
@@ -1,5 +1,5 @@
 
-package com.reactlibrary;
+package com.toyberman.drawOverlay;
 
 import java.util.Arrays;
 import java.util.Collections;
